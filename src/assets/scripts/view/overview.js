5758f068bf5de72797cab3f90d25aa58a810b762
lSoleyl <lSoleyl+github@gmail.com>
2015-11-11 17:56:00 +0100

    Added two new charts: By Currency and By Continent  * Added description row for charts  * Fixed redisplay issue by waiting a tick before creating the charts to let the dom calculate the correct canvas sizes

--- src/assets/scripts/view/overview.js
+++ src/assets/scripts/view/overview.js
@@ -1,6 +1,25 @@
-define(['jquery', 'chart', 'd3', 'portfolio'], function($, Chart, d3, portfolio) {
+define(['jquery', 'chart', 'd3', 'async', 'portfolio'], function($, Chart, d3, async, portfolio) {
   var view = {}
 
+
+  var filters = [
+    {
+      key:'type',
+      name:'Type'
+    },
+    {
+      key: function(x) { return x.price.currency },
+      name: 'Currency'
+    },
+    {
+      key: function(x) { return x.region.continent },
+      name: 'Continent',
+      next: {
+        key: function(x) { return x.region.country },
+        name: 'Country'
+      }
+    }
+  ]
 
 
   var colors = _.map(d3.scale.category20().range(), function(color) { 
@@ -15,49 +34,58 @@ define(['jquery', 'chart', 'd3', 'portfolio'], function($, Chart, d3, portfolio)
     return colors[++ci % colors.length]
   }
 
-
-
-  var charts = []
-
   view.onShow = function() {
+
     var content = $("#overviewContentDiv")
-    content.empty() //Remove all old data
-    _.each(charts, function(chart) { chart.destroy() }) //Clear out old charts
-    charts = []
+    content.empty()
+    
+    
+
 
     if (portfolio.assets.length == 0) {
-      content.append('<div class="alert alert-warning" role="alert">Portfolio is empty, add assets first</div>')
+      content.append($('<div class="alert alert-warning" role="alert">Portfolio is empty, add assets first</div>'))
       return
     }
 
-    var row = $('<div class="row"></div>')
 
-    //Add canvas, which groups portfolio by type
-    var column = $('<div class="col-md-4"></div>')
-    var canvas = $('<canvas></canvas>')
-    column.append(canvas)
-    row.append(column)
-    content.append(row)
+    var titleRow = $('<div class="row"></div>')
+    var canvasRow = $('<div class="row"></div>')
+    content.append(titleRow)
+    content.append(canvasRow)
 
-    var ctx = canvas[0].getContext("2d")
-    var chart = new Chart(ctx).Pie()
-    charts.push(chart)
+    _.each(filters, function(filter) {
+      var title = $('<div class="col-md-4"><center>By ' + filter.name + '</center></div>')
+      var canvas = $('<canvas></canvas>')
 
+      title.appendTo(titleRow)
+      canvas.appendTo('<div class="col-md-4">').appendTo(canvasRow)
 
-    var grouped = _.groupBy(portfolio.assets, 'type')
-    var segments = _.map(grouped, function(assets, type) {
-      return _.assign({
-        value: _.sum(assets, function(asset) { return asset.volume() }).toFixed(2),
-        label: type
-      }, getColor())
+      async.nextTick(function() { //Canvas is not immediately ready
+
+        var ctx = canvas[0].getContext("2d")
+        var chart = new Chart(ctx).Pie()
+        var accessor = filter.key
+        if (typeof accessor == "string") {
+          accesskey = accessor
+          accessor = function(x) { return x[accesskey] }
+        }
+
+        ci = 0 //Reset color index for each diagram
+        var grouped = _.groupBy(portfolio.assets, accessor)
+        var segments = _.map(grouped, function(assets, type) {
+          return _.assign({
+            value: _.sum(assets, function(asset) { return asset.volume() }).toFixed(2),
+            label: type
+          }, getColor())
+        })
+
+        _.each(segments, function(segment) { chart.addData(segment) })
+        chart.update()
+      })
     })
-
-    _.each(segments, function(segment) { chart.addData(segment) })
-    chart.update()
-
-    //TODO add other chart types
-    //TODO redraw correctly after switching page
-    //TODO change labels
+   
+   
+    
     //TODO add click listener
 
   }
