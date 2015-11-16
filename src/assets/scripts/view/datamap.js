67a87106bddf4b47ab6df0b1e16e0a95aa3abd3b
pasndrp <p.assendorp@gmail.com>
2015-11-17 00:04:50 +0100

    DataMap updated to show Diversity of Portfolio

--- src/assets/scripts/view/datamap.js
+++ src/assets/scripts/view/datamap.js
@@ -1,9 +1,52 @@
-define(['topojson', 'd3', 'datamaps'], function(t,d, Datamap) {
+define(['topojson', 'd3', 'lodash', 'datamaps', 'model', 'portfolio'], function(t,d, _, Datamap, model, portfolio) {
   var view = {}
 
   view.onLoad = function() {
+
+    // Thresholds for Coloring
+    var lightThreshold = 0;
+    var mediumThreshold = 0.05;
+    var heavyThreshold = 0.1;
+
+    // Amount of Assets in Portfolio
+    var totalAssets = portfolio.assets.length
+
+    // Prepare Country Data for Visualization
+    var data = {}
+    _.each(model.countries, function(country){
+
+      // Fetch Assets for given Country from Portfolio
+      var assets = portfolio.assetsByCountry(country.id)
+      var fillKey = "defaultFill";
+
+      // Determine FillKey by CountryAssets/TotalAssets Ratio
+      var assetRatio = assets? assets.length/totalAssets : 0
+      if (assetRatio > heavyThreshold){
+        fillKey = "heavy"
+      } else if (assetRatio > mediumThreshold){
+        fillKey = "medium"
+      } else if (assetRatio > lightThreshold){
+        fillKey = "light"
+      }
+
+      data[country.id] = { fillKey: fillKey, assets: assets.length }
+    })
+
     this.worldMap = new Datamap({
-      element: document.getElementById("worldmapDiv")
+      element: document.getElementById("worldmapDiv"),
+      projection: 'mercator',
+      geographyConfig: {
+        popupTemplate: function(geography, data) {
+          return '<div class="hoverinfo"><strong>' + geography.properties.name + '</strong><br>Assets: ' +  data.assets + ' '
+        }
+      },
+      fills: {
+        defaultFill: "lightgrey",
+        light: "#65ae75",
+        medium: "#388e3c",
+        heavy: "#286118"
+      },
+      data: data
     })
   }
 
