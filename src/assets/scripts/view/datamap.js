90e716e8be1e06e6973f3b0d0217a86dce0c5a6e
pasndrp <p.assendorp@gmail.com>
2015-11-18 11:39:31 +0100

    DataMap dynamic ColorRange and auto reload of Assets added

--- src/assets/scripts/view/datamap.js
+++ src/assets/scripts/view/datamap.js
@@ -1,13 +1,43 @@
 define(['topojson', 'd3', 'lodash', 'datamaps', 'model', 'portfolio'], function(t,d, _, Datamap, model, portfolio) {
   var view = {}
 
+  // Color Range
+  var minColor = "#F5F5F5"
+  var maxColor = "#208208"
+
   view.onShow = function() {
 
-    // Thresholds for Coloring
-    var lightThreshold = 0;
-    var mediumThreshold = 0.05;
-    var heavyThreshold = 0.1;
+    // Generate WorldMap Data
+    var data = generateMapData()
 
+    console.log(data)
+
+    if(this.map == undefined){
+
+      this.map = new Datamap({
+        element: document.getElementById("worldmapDiv"),
+        projection: 'mercator',
+        geographyConfig: {
+            borderColor: '#DEDEDE',
+            // show desired information in tooltip
+            popupTemplate: function(geography, data) {
+              return '<div class="hoverinfo"><strong>' + geography.properties.name + '</strong><br>Assets: ' +  data.assets + ' '
+            }
+        },
+        fills: { defaultFill: '#F5F5F5' },
+        data: data
+      })
+
+    } else {
+      // Update Infos on Map
+      this.map.updateChoropleth(data)
+    }
+  }
+
+  /**
+   * Generate Data for Countries to be displayed on WorldMap
+   */
+  function generateMapData(){
     // Amount of Assets in Portfolio
     var totalAssets = portfolio.assets.length
 
@@ -15,50 +45,35 @@ define(['topojson', 'd3', 'lodash', 'datamaps', 'model', 'portfolio'], function(
     var data = {}
     _.each(model.countries, function(country){
 
-      // Fetch Assets for given Country from Portfolio
-      var assets = portfolio.assetsByCountry(country.id)
-      var fillKey = "defaultFill";
+      if(country.id != -99){
+        // Fetch Assets for given Country from Portfolio
+        var assets = portfolio.assetsByCountry(country.id)
+        var fillKey = "defaultFill"
 
-      // Determine FillKey by CountryAssets/TotalAssets Ratio
-      var assetRatio = assets? assets.length/totalAssets : 0
-      if (assetRatio > heavyThreshold){
-        fillKey = "heavy"
-      } else if (assetRatio > mediumThreshold){
-        fillKey = "medium"
-      } else if (assetRatio > lightThreshold){
-        fillKey = "light"
+        data[country.id] = { assets: assets.length }
       }
-
-      data[country.id] = { fillKey: fillKey, assets: assets.length }
     })
-    if(this.worldMap == undefined) {
-      this.worldMap = new Datamap({
-        element: document.getElementById("worldmapDiv"),
-        projection: 'mercator',
-        geographyConfig: {
-          popupTemplate: function(geography, data) {
-            return '<div class="hoverinfo"><strong>' + geography.properties.name + '</strong><br>Assets: ' +  data.assets + ' '
-          }
-        },
-        fills: {
-          defaultFill: "lightgrey",
-          light: "#65ae75",
-          medium: "#388e3c",
-          heavy: "#286118"
-        },
-        data: data
-      })
-    } else {
-      for (var key in data) {
-        var newData = {};
-        if (key != -99) {
-          newData[key] = {
-            fillKey: data[key].fillKey
-          };
-          this.worldMap.updateChoropleth(newData);
-        }
-      }
-    }
+
+    // We need to colorize every country based on number of Assets
+    // colors should be unique for every value.
+    // For this purpose we create palette(using min/max series-value)
+    var assetValues = _.map(data, function(item){ return item.assets; })
+    var minValue = Math.min.apply(null, assetValues),
+        maxValue = Math.max.apply(null, assetValues)
+
+    // create color palette function
+    // color can be whatever you wish
+    var paletteScale = d3.scale.linear()
+            .domain([minValue, maxValue])
+            .range([minColor, maxColor])
+
+    // fill dataset in appropriate format
+    _.each(model.countries, function(country){
+      if(country.id != -99)
+        data[country.id].fillColor = paletteScale(data[country.id].assets)
+    })
+
+    return data
   }
 
   return view
