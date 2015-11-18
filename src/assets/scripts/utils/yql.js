fcf1cc82891c0aefef72450e7e1bd4b65056050b
lSoleyl <lSoleyl+github@gmail.com>
2015-11-18 12:18:06 +0100

    YQL uses protocol, which was used to open moneypie to work for both: http and https

--- src/assets/scripts/utils/yql.js
+++ src/assets/scripts/utils/yql.js
@@ -2,8 +2,8 @@
  */
 define(['jquery'], function($) {
   return function(url, cb) {
-
-    var yqlurl = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url=%22" + url + "%22%20and%20xpath=%22*%22"
+    
+    var yqlurl = document.location.protocol + "//query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url=%22" + url + "%22%20and%20xpath=%22*%22"
     $.ajax(yqlurl, { 
       dataType:"text",
       error: function(x,t,err) { return cb(err) },
